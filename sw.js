771fc5e42df7e384fe579a914b29ece8217597ba
tobusaiseikai-ce <o.yuuki0817@gmail.com>
2026-05-19 21:32:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GitHub Pages: https://tobusaiseikai-ce.github.io/device-map/
-const CACHE_NAME = 'dokodeba-v1';
+const CACHE_NAME = 'dokodeba-v2';
 const ASSETS = [
   '/device-map/',
   '/device-map/index.html',
@@ -25,6 +25,12 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
